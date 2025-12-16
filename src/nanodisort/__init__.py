ed421d2b8ad8be88696b45d43a83662f46b9efb1
Vincent Leroy <vincent.leroy@rayference.eu>
2025-12-16 15:00:50 +0100

    Minor updates to docstrings

--- src/nanodisort/__init__.py
+++ src/nanodisort/__init__.py
@@ -18,6 +18,8 @@ Examples
 >>> state.solve()
 """
 
+from __future__ import annotations
+
 import itertools
 
 from nanodisort._core import DisortState as _DisortState
@@ -28,9 +30,29 @@ from ._version import _version as __version__
 class DisortState(_DisortState):
     """
     DISORT solver state.
+
+    This class provides an interface to the CDISORT radiative transfer model.
+    CDISORT is a C port of the original DISORT Fortran package. The main
+    differences are:
+
+    * **Improved memory management.** CDISORT allocates memory dynamically to
+      use memory buffers of the right size, and internally encapsulates data
+      in structs. This overall improves performance compared to the Fortran
+      implementation.
+    * **Double precision.** CDISORT operates entirely with double-precision
+      arithmetics for improved numerical stability.
+    * **Correction of intensity fields** by Buras-Emde algorithm, included by
+      Robert Buras.
+    * **Pseudospherical geometry** for direct beam source, included by Arve
+      Kylling.
+    * **Solution for a general source term**, included by Arve Kylling.
     """
 
     def __init__(self) -> None:
+        """
+        Initialize the DISORT state. All parameters are initialized with their
+        default values.
+        """
         super().__init__()
 
     def print_state(self, pad: int | None = 0) -> None:
